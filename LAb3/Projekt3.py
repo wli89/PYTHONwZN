bab35f8142d22649df1d0e9bf14fc17dd4810a03
wli89 <64853819+wli89@users.noreply.github.com>
2021-11-30 01:59:42 +0100

    Add files via upload

---
+++ LAb3/Projekt3.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 14:46:36 2021
+
+@author: weron
+"""
+
+import json
+import argparse
+import requests
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser(description = "Opis")
+parser.add_argument('-file', help = 'file name', type = str, default = 'Filmy')
+args = parser.parse_args()
+
+data = []
+
+req = requests.get('https://zalukaj.cc/')
+
+soup = BeautifulSoup(req.text, 'html.parser')
+print(soup.title.text)
+
+divs_films = soup.find('div', id = 'tab-1')
+divs_films_2 = divs_films.find_all('div', class_ = 'wrapper')
+
+for film in divs_films_2:
+    
+    title = film.find('h3', class_ = "title")
+    title2 = title.find('a').text
+    genere = film.find('div', class_ = 'genere')
+    viwes = film.find('div', class_ = 'views').text
+    version = film.find('div', class_ = 'version')
+    version2 = version.find('div', class_ = 'tooltip').text
+    
+    show = {"TYTUL:": title2, "ROK:": genere, "WIDZIANO:":viwes, "INFO:": version2}
+    data.append(show)
+
+with open(f'{args.file}.json', "w", encoding = "utf-8") as f:
+    json.dump(data, f)
+   
